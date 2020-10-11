13a347ef7c51f3c60d55e771c2176f9ef40a9ef3
Danylo Hrudzynskyi <daniil.grudzinsky@gmail.com>
2020-10-11 21:29:16 +0300

    add gender

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace EFExampleBasic
 {
     public class Program
     {
-        public static string ConnectionString = "Server=localhost\\sqlexpress;Database=EFExampleBasic;Integrated Security=true;MultipleActiveResultSets=True;";
+        public static string ConnectionString = "Server=localhost\\sqlexpress;Database=EFExampleBasicDeveloper1;Integrated Security=true;MultipleActiveResultSets=True;";
 
         static void Main(string[] args)
         {
